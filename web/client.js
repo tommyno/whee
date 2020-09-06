0846d19a5ed9dfaea77302559c305592290239bf
tommyno <tommy@spindelvev.net>
2020-09-06 20:59:58 +0200

    Add eslint

--- web/client.js
+++ web/client.js
@@ -3,5 +3,5 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "38sqgmq0",
   dataset: "production",
-  useCdn: false, // Change to true when in production
+  useCdn: false // Change to true when in production
 });
