c60550cd00f5cd0badec7ae2011e141508fe9f90
tommyno <tommy@spindelvev.net>
2020-09-18 11:58:47 +0200

    Add social media icons to footer

--- web/components/Footer/Footer.js
+++ web/components/Footer/Footer.js
@@ -75,30 +75,32 @@ const Footer = ({ frontpage }) => {
               <a href="tel:22120068">22 12 00 68</a>
             </p>
             <p>
-              <a
-                href="https://www.instagram.com/wheebike/"
-                title="Whee! på Instagram"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/instagram.png" alt="Instagram ikon" />
-              </a>
-              <a
-                href="https://www.facebook.com/wheebike/"
-                title="Whee! på Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/facebook.png" alt="Facebook ikon" />
-              </a>
-              <a
-                href="https://www.youtube.com/c/wheebike"
-                title="Whee! på YouTube"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/youtube.png" alt="Youtube ikon" />
-              </a>
+              <div className={styles.some}>
+                <a
+                  href="https://www.instagram.com/wheebike/"
+                  title="Whee! på Instagram"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/instagram.png" alt="Instagram ikon" />
+                </a>
+                <a
+                  href="https://www.facebook.com/wheebike/"
+                  title="Whee! på Facebook"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/facebook.png" alt="Facebook ikon" />
+                </a>
+                <a
+                  href="https://www.youtube.com/c/wheebike"
+                  title="Whee! på YouTube"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/youtube.png" alt="Youtube ikon" />
+                </a>
+              </div>
             </p>
           </Flow>
         </Grid>
