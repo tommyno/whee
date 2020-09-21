9548a60baa327ed221e4b20e70b663a89849e0ec
tommyno <tommy@spindelvev.net>
2020-09-21 10:21:08 +0200

    Remove unused getInitalProps

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -2,11 +2,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { GA_TRACKING_ID } from "utils/gtag";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="no">
