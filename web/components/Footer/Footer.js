656b1a5e934bc42e7d1be81264ac1be74a3c4979
tommyno <tommy@spindelvev.net>
2020-09-22 00:28:45 +0200

    Update footer images

--- web/components/Footer/Footer.js
+++ web/components/Footer/Footer.js
@@ -13,10 +13,10 @@ const Footer = ({ frontpage }) => {
   });
 
   return (
-    <footer className={footerClass}>
-      {frontpage && (
+    <>
+      {/* {frontpage && (
         <img
-          src="/images/footer-parade.png"
+          src="/images/footer-tall.png"
           alt=""
           className={styles.footerImage}
         />
@@ -24,88 +24,95 @@ const Footer = ({ frontpage }) => {
 
       {!frontpage && (
         <img
-          src="/images/footer-parade-slim.png"
+          src="/images/footer-slim.png"
           alt=""
           className={styles.footerImage}
         />
-      )}
-      <Section outer="none" inner="medium" color="red">
-        <Grid>
-          <nav>
+        )} */}
+      <footer className={footerClass}>
+        <img
+          src="/images/footer-slim.png"
+          alt=""
+          className={styles.footerImage}
+        />
+        <Section outer="none" inner="large" color="red">
+          <Grid>
+            <nav>
+              <Flow>
+                <p>
+                  <Link href="/sykkelen">
+                    <a className="link-underline">Sykkelen</a>
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/tjenesten">
+                    <a className="link-underline">Tjenesten</a>
+                  </Link>
+                </p>
+                <p>
+                  <Button link="/forhandsbestill" menu>
+                    Forhåndsbestill nå
+                  </Button>
+                </p>
+              </Flow>
+            </nav>
+
+            <nav>
+              <Flow>
+                <p>
+                  <Link href="/hvem-er-whee">
+                    <a className="link-underline">Hvem er Whee!?</a>
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/personvern-og-cookies">
+                    <a className="link-underline">Personvern og cookies</a>
+                  </Link>
+                </p>
+              </Flow>
+            </nav>
+
             <Flow>
               <p>
-                <Link href="/sykkelen">
-                  <a className="link-underline">Sykkelen</a>
-                </Link>
+                <a href="mailto:hei@whee.no" className="link-underline">
+                  hei@whee.no
+                </a>
               </p>
               <p>
-                <Link href="/tjenesten">
-                  <a className="link-underline">Tjenesten</a>
-                </Link>
+                <a href="tel:22120068">22 12 00 68</a>
               </p>
-              <p>
-                <Button link="/forhandsbestill" menu>
-                  Forhåndsbestill nå
-                </Button>
-              </p>
+
+              <div className={styles.some}>
+                <a
+                  href="https://www.instagram.com/wheebike/"
+                  title="Whee! på Instagram"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/instagram.png" alt="Instagram ikon" />
+                </a>
+                <a
+                  href="https://www.facebook.com/wheebike/"
+                  title="Whee! på Facebook"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/facebook.png" alt="Facebook ikon" />
+                </a>
+                <a
+                  href="https://www.youtube.com/c/wheebike"
+                  title="Whee! på YouTube"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/youtube.png" alt="Youtube ikon" />
+                </a>
+              </div>
             </Flow>
-          </nav>
-
-          <nav>
-            <Flow>
-              <p>
-                <Link href="/hvem-er-whee">
-                  <a className="link-underline">Hvem er Whee!?</a>
-                </Link>
-              </p>
-              <p>
-                <Link href="/personvern-og-cookies">
-                  <a className="link-underline">Personvern og cookies</a>
-                </Link>
-              </p>
-            </Flow>
-          </nav>
-
-          <Flow>
-            <p>
-              <a href="mailto:hei@whee.no" className="link-underline">
-                hei@whee.no
-              </a>
-            </p>
-            <p>
-              <a href="tel:22120068">22 12 00 68</a>
-            </p>
-
-            <div className={styles.some}>
-              <a
-                href="https://www.instagram.com/wheebike/"
-                title="Whee! på Instagram"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/instagram.png" alt="Instagram ikon" />
-              </a>
-              <a
-                href="https://www.facebook.com/wheebike/"
-                title="Whee! på Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/facebook.png" alt="Facebook ikon" />
-              </a>
-              <a
-                href="https://www.youtube.com/c/wheebike"
-                title="Whee! på YouTube"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/youtube.png" alt="Youtube ikon" />
-              </a>
-            </div>
-          </Flow>
-        </Grid>
-      </Section>
-    </footer>
+          </Grid>
+        </Section>
+      </footer>
+    </>
   );
 };
 
