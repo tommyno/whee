235579aa7fdc6d451dde5495943f07c9ca180974
tommyno <tommy@spindelvev.net>
2020-09-22 09:09:39 +0200

    Set Sanity to use CDN

--- web/settings/client.js
+++ web/settings/client.js
@@ -3,5 +3,5 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "38sqgmq0",
   dataset: "production",
-  useCdn: false // Change to true when in production
+  useCdn: true // Change to true when in production
 });
