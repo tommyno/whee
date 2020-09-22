2c7391593daf84d7a70b0cbfffe34d0a32023f10
tommyno <tommy@spindelvev.net>
2020-09-22 10:02:48 +0200

    Add google site verification code

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -26,6 +26,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta
+          name="google-site-verification"
+          content="M7fEOP5BBe6sat3elSdQ6tLiODS_OQTbCz4pC2EN8x4"
+        />
 
         <link
           rel="apple-touch-icon"
