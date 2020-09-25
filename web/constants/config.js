5a7c721a7678f20ba7b9c82b1dac4d4df30e27a6
tommyno <tommy@spindelvev.net>
2020-09-25 09:57:01 +0200

    Disable Google Analytics for localhost

---
+++ web/constants/config.js
@@ -0,0 +1,5 @@
+const config = {
+  GA_TRACKING_ID: "UA-178543994-1"
+};
+
+export default config;
--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,30 +1,44 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "utils/gtag";
+import config from "constants/config";
+
+// Insert Google Analytics to head
+const GoogleAnalytics = () => {
+  // Disable for localhost / dev
+  const isLocalhost = process.env.NEXT_PUBLIC_IS_LOCALHOST;
+  if (isLocalhost) {
+    return null;
+  }
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${config.GA_TRACKING_ID}`}
+      />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${config.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `
+        }}
+      />
+    </>
+  );
+};
 
 class MyDocument extends Document {
   render() {
-    // eslint-disable-next-line no-return-assign
     return (
       <Html lang="no">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-            }}
-          />
+          <GoogleAnalytics />
         </Head>
         <body>
           <Main />
