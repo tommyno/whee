0de439aa52851418ef131136dc2e0342a7c9fa44
tommyno <tommy@spindelvev.net>
2020-09-25 10:41:25 +0200

    Change description for links

--- studio/schemas/parts/link.js
+++ studio/schemas/parts/link.js
@@ -1,7 +1,7 @@
 export default {
   name: "href",
   title: "Lenke",
-  description: "Bruk formatet: /fortroppen for internlenker. Bruk fullstendig url for eksternlenker, feks: https://sykkel.no/hjul. For e-post, bruk: mailto:hei@whee.no",
+  description: "Bruk formatet: /sykkelen for internlenker. Bruk fullstendig url for eksternlenker, feks: https://sykkel.no/hjul. For e-post, bruk: mailto:hei@whee.no",
   type: "url",
   validation: (Rule) =>
     Rule.uri({
--- studio/schemas/parts/richTextItem.js
+++ studio/schemas/parts/richTextItem.js
@@ -35,7 +35,7 @@ export default {
           {
             name: "link",
             type: "object",
-            title: "Ekstern lenke",
+            title: "Lenke",
             fields: [link],
           },
         ],
