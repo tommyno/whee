dba24168b4f6dee1d8765b76203a423b86b395ca
tommyno <tommy@spindelvev.net>
2020-10-30 12:18:06 +0100

    Add working auth + profile fetch

---
+++ web/pages/api/user/profile.js
@@ -0,0 +1,57 @@
+// Get user profile
+import Cookies from "cookies";
+import jwt from "jsonwebtoken";
+
+export default async (req, res) => {
+  try {
+    // Get auth token from cookie
+    const cookies = new Cookies(req, res);
+    const authToken = cookies.get("authToken");
+
+    // Verify auth token
+    // Throw error if jwt token with mobile number has been tampered or has expired
+    let decoded;
+    try {
+      decoded = jwt.verify(authToken, process.env.JWT_SECRET);
+    } catch (error) {
+      throw new Error("Ugyldig token");
+    }
+    if (!decoded) {
+      throw new Error("Ugyldig token");
+    }
+
+    // Extract verified number from decoded jwt
+    const { number } = decoded?.data;
+
+    // Get data from Airtable
+    const base = "appvw0UePoh0VxvkB";
+    const table = "Kunder";
+    const url = `https://api.airtable.com/v0/${base}/${table}?filterByFormula%3D%22Mobil%3D%22${number}%22`;
+
+    const response = await fetch(url, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+      }
+    });
+
+    // Handle errors
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    // Return only the data we need to expose
+    const result = await response.json();
+    const {
+      Fornavn: firstName = "",
+      Etternavn: lastName = "",
+      Epost: email = ""
+    } = result?.records[0]?.fields;
+
+    // All good
+    res.status(200).json({ firstName, lastName, email });
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
--- web/pages/min-side.js
+++ web/pages/min-side.js
@@ -1,10 +1,15 @@
+import PropTypes from "prop-types";
+
 import Seo from "utils/seo";
 
 import { Section, Block, Flow } from "components/Layout";
 import Footer from "components/Footer";
 import Header from "components/Header";
+import { Router } from "next/router";
 
-const MyPage = () => {
+const MyPage = ({ user }) => {
+  const { firstName } = user;
+
   return (
     <>
       <Seo page={{ title: "Min side" }} noindex nofollow />
@@ -14,7 +19,7 @@ const MyPage = () => {
       <Section limitedWidth outer="firstSection">
         <Block bottom={7}>
           <Flow>
-            <h1>Min side</h1>
+            <h1>Velkommen {firstName}</h1>
             <p>Innhold kommer her</p>
           </Flow>
         </Block>
@@ -23,6 +28,50 @@ const MyPage = () => {
       <Footer />
     </>
   );
+};
+
+export async function getServerSideProps(ctx) {
+  const { cookie = "" } = ctx.req.headers;
+
+  // Fetch customer data
+  const url = `${process.env.BASEURL}/api/user/profile`;
+  const response = await fetch(url, {
+    headers: {
+      cookie
+    }
+  });
+  let result = {};
+
+  // Not authenticated
+  // Client side
+  if (response.status === 401 && !ctx.req) {
+    Router.replace("/login");
+  }
+
+  // Not authenticated
+  // Server side
+  if (response.status === 401 && ctx.req) {
+    ctx.res?.writeHead(302, {
+      Location: `${process.env.BASEURL}/login`
+    });
+    ctx.res?.end();
+  }
+
+  if (response.ok) {
+    result = await response.json();
+  }
+
+  return {
+    props: {
+      user: {
+        ...result
+      }
+    }
+  };
+}
+
+MyPage.propTypes = {
+  user: PropTypes.object.isRequired
 };
 
 export default MyPage;
